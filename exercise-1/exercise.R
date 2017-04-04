eccbec23811fbcfcc76844ec0fb4bd2808bfd385
AnnieeeeC <changy42@uw.edu>
2017-04-04 14:56:35 -0700

    modified

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,9 +1,8 @@
 # Exercise 1: writing and executing functions
 
 # Write a function `AddThree` that adds 3 to an input value
-AddThree <- function(a, b, c) (
-  answer <- a + b + c
-  return (answer)
+AddThree <- function(a) (
+  return (a + 3)
 )
 
 # Create a variable `ten` by passing 7 to your `AddThree` function
